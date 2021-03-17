4822d3edde4c0855b596927f8f5164ad28e0c486
clemkeirua <clement@keiruaprod.fr>
2021-03-17 16:42:32 +0100

    removed unused socket code

--- current_version/build.sh
+++ current_version/build.sh
@@ -2,4 +2,4 @@
 # apt-get install libsdl-dev libdevil-dev
 # http://ftp.debian.org/debian/pool/main/d/devil/libdevil-dev_1.7.8-10+b2_amd64.deb
 # sudo dpkg -i libdevil-dev_1.7.8-10+b2_amd64.deb
-g++ -I./K2D -I/usr/include/SDL -I/usr/include/GL -I/usr/include main.cpp K2D/K2D_Engine.h
+g++ -I./K2D -I/usr/include/SDL -I/usr/include/GL -I/usr/include main.cpp K2D/K2D_Engine.h CGame.h
